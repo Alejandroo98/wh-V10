a798effe5b8783336ff52b354612528cfcead49c
Alejandro <jeffersonaleandro27@gmail.com>
2022-04-28 18:34:41 -0500

    cambio de hora - handleTime.js

--- routes/web.js
+++ routes/web.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getQr } = require('../helpers/web');
 
-router.use('/qr', getQr);
+// router.use('/qr', getQr);
 
 module.exports = router;
