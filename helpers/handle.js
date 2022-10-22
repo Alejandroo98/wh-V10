bf2c0728e8e6454584e39bb1eca81070b607544b
alejandroo98 <jeffersonalejandro27@gmail.com>
2022-10-22 00:28:38 -0500

    Solucion de error

--- helpers/handle.js
+++ helpers/handle.js
@@ -3,9 +3,9 @@ const { LegacySessionAuth } = require('whatsapp-web.js');
 const MULTI_DEVICE = process.env.MULTI_DEVICE || 'true';
 
 const cleanNumber = (number) => {
-  number = number.replace('@c.us', '');
-  number = `${number}@c.us`;
-  return number;
+	number = number.replace('@c.us', '');
+	number = `${number}@c.us`;
+	return number;
 };
 
 /**
@@ -14,33 +14,35 @@ const cleanNumber = (number) => {
  * @param {*} cb
  */
 const createClient = (session = {}, login = false) => {
-  console.log(`Mode: ${MULTI_DEVICE === 'false' ? 'No Multi-device' : 'Si Multi-device'} `);
+	console.log(
+		`Mode: ${MULTI_DEVICE === 'false' ? 'No Multi-device' : 'Si Multi-device'} `
+	);
 
-  const objectLegacy = login
-    ? {
-        authStrategy: new LegacySessionAuth({
-          session,
-        }),
-      }
-    : { session };
+	const objectLegacy = login
+		? {
+				authStrategy: new LegacySessionAuth({
+					session,
+				}),
+		  }
+		: { session };
 
-  if (MULTI_DEVICE == 'false') {
-    return {
-      ...objectLegacy,
-      restartOnAuthFail: true,
-      puppeteer: {
-        args: ['--no-sandbox'],
-      },
-    };
-  } else {
-    return {
-      puppeteer: {
-        headless: true,
-        args: ['--no-sandbox'],
-      },
-      clientId: 'client-one',
-    };
-  }
+	if (MULTI_DEVICE == 'false') {
+		return {
+			...objectLegacy,
+			restartOnAuthFail: true,
+			puppeteer: {
+				args: ['--no-sandbox'],
+			},
+		};
+	} else {
+		return {
+			puppeteer: {
+				headless: true,
+				args: ['--no-sandbox'],
+			},
+			clientId: 'client-one',
+		};
+	}
 };
 
 module.exports = { cleanNumber, createClient };
